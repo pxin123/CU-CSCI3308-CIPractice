a7e2018876131a0b6d4eed6cb00b063822f44def
pxin123 <pexi5825@colorado.edu>
2017-04-20 17:54:05 -0700

    Update geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -145,27 +145,11 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
-START_TEST(test_2d_area_triangle)
-{
-    coord_2d_t a;
-    coord_2d_t b;
-    coord_2d_t c;
-
-    a.x = 1;
-    a.y = 1;
-    b.x = 11;
-    b.y = 0;
-    c.x = 50;
-    c.y = 25;
-    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 144.0);
-
-}
-END_TEST
-
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
 
+    /* Create Suite */
     Suite* s = suite_create("coord_2d");
 
     /* Setup Test Cases */
@@ -178,14 +162,10 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
-    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
-    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
-
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
