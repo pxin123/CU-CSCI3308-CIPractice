744342a0a85186e1ea22fa21f5ac05491d55c6c2
pxin123 <pexi5825@colorado.edu>
2017-04-20 18:20:01 -0700

    Update geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -12,7 +12,7 @@
 
 #include <stdlib.h>
 #include <check.h>
-
+#include <stdio.h>
 #include "geometry.h"
 
 /* coord_2d_eq Test */
@@ -145,36 +145,26 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
-/* coord_2d_triangle area */
-START_TEST(test_2d_area)
+/* coord_2d_area_triangle Test */
+START_TEST(test_2d_area_triangle)
 {
     coord_2d_t a;
     coord_2d_t b;
     coord_2d_t c;
-    double exp_area;
-    double area;
-    int flag;
 
-    a.x = b.x = 0;
-	c.x = 5;
-    a.y = c.y = 0;
-    b.y = 3;
-    area = coord_2d_area_triangle(&a, &b, &c);
-    exp_area = 7.5;
-    //printf("THE AREA IS: %f", area);
-    if (area == exp_area) {
-		//printf("HEREEEEEE");
-		flag = true;
-	}
-	else{
-		//printf("HEREEEEEE FLASE");
-		flag = false;
-	}
-    ck_assert(flag);
+    a.x = 0.0;
+    a.y = 0.0;
+    b.x = 0.0;
+    b.y = 0.0;
+    c.x = 0.0;
+    c.y = 0.0;
+
+    //printf("test:%f\n", coord_2d_area_triangle(&a, &b,&c));
+    ck_assert(coord_2d_area_triangle(&a, &b,&c) == 0.0);
+
+
 }
 END_TEST
-
-
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -191,15 +181,14 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
-    
-    TCase* tc_2d_area = tcase_create("coord_2d_area_triangle");
-    tcase_add_test(tc_2d_area, test_2d_area);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-    suite_add_tcase(s, tc_2d_area);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
